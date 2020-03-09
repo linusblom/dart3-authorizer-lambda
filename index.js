ada41cd988dfbfa762d907c5449941e27f5ffe4c
Linus Blom <linus.blom@me.com>
2020-03-09 19:09:42 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const lib = require("./lib");
+let data;
+
+// Lambda function index.handler - thin wrapper around lib.authenticate
+module.exports.handler = async event => {
+  try {
+    data = await lib.authenticate(event);
+  } catch (err) {
+    console.log(err);
+    throw Error("Unauthorized");
+  }
+  return data;
+};
